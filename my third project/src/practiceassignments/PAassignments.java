31f861aa49abec726327407079978a87673f59c2
samita maggo <simmii.mg@gmail.com>
2023-09-19 17:21:56 -0500

    PA assignment 1st problem

---
+++ my third project/src/practiceassignments/PAassignments.java
@@ -0,0 +1,16 @@
+package practiceassignments;
+
+public class PAassignments {
+
+
+ public static void main(String[]args) {
+   int a = 10;
+   int b = 20;
+   int c = a+b;
+   System.out.println("result = "+ c );
+	   
+   }
+		   
+	
+	
+ }

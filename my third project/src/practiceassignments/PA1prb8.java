08251cdde19fced67116677d65f93784c8928cc2
samita maggo <simmii.mg@gmail.com>
2023-09-20 10:15:48 -0500

    PAprblm8

---
+++ my third project/src/practiceassignments/PA1prb8.java
@@ -0,0 +1,17 @@
+package practiceassignments;
+
+public class PA1prb8 {
+	public static void main(String [] args) {
+	 double coffee = 5.99;
+	 double cakeslice = 3.99;
+	 double latte = 6.99;
+	 double subtotal;
+	 double Totalamount;
+	 final double salestax = 7.99;
+	 subtotal= coffee*2+cakeslice*2+latte*3;
+	 Totalamount = subtotal+ salestax;
+	 System.out.println( Totalamount);
+	 
+	 
+ }
+}

a62a40f4a8b3b9b3a7713b53988c9c6574f30128
samita maggo <simmii.mg@gmail.com>
2023-09-20 08:47:10 -0500

    my PA practice

---
+++ my third project/src/practiceassignments/PA1prb3.java
@@ -0,0 +1,10 @@
+package practiceassignments;
+
+public class PA1prb3 {
+ public static void main(String[]args) {
+	 int x = 5;
+	 double y = 4.5;
+	 double z = x + y;
+	 System.out.println(" result " + z);
+ }
+}
---
+++ my third project/src/practiceassignments/doublevariationaddition.java
@@ -0,0 +1,19 @@
+package practiceassignments;
+
+public class doublevariationaddition {
+ public static void main(String[] args) {
+ double x = 0 ;
+ double y = 0 ;
+ double z;
+  x = 2.345;
+  y = 3.657 ; 
+  z = x + y;
+  System.out.println("result z =" + z);
+ }
+		  
+ 
+ 
+		 
+ 
+ 
+}

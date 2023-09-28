9dadfe4b4649e271251d6b19c22ee6d506944be8
samita maggo <simmii.mg@gmail.com>
2023-09-28 17:38:16 -0500

    fixing project structure

---
+++ mythirdproject/src/practiceassignments/Leapyearprbpg45.java
@@ -0,0 +1,43 @@
+package practiceassignmemts;
+
+import java.util.Scanner;
+
+public class Leapyearprbpg45 {
+	public static void  main(String[] args) {
+		int year;
+		
+		Scanner scan = new Scanner(System.in);
+				
+		
+		System.out.println( " Enter the year");
+		
+		year = scan.nextInt();
+		
+		if((year % 4==0)&&(year% 100== 0)||(year%100!=0 && year%4 == 0)) { 
+			System.out.println(" This year is leap year");
+			
+		}else {
+		
+		System.out.println(" This is not a leap year");
+	}
+}
+}
+			
+			
+		
+	
+	 
+		
+			
+		
+		
+		
+		
+	
+
+	
+
+	
+
+
+
